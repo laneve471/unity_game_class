856900432348c914a470c498e5b9e8f5344de326
Joo Jeongeun <jusally03@naver.com>
2024-01-31 23:47:18 +0900

    캐릭터 이동 불완전하게 구현, 맵과 캐릭터 초기 모델링/뼈대 제작

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed=5f;
+    [SerializeField] float mouseSpeed = 8f;
+    private float gravity;
+    private CharacterController controller;
+    private Vector3 mov;
+    public float jumpForce;
+    private float verticalVelocity;
+    
+
+    private float mouseX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        mov = Vector3.zero;
+        gravity = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X") * mouseSpeed;
+        this.transform.localEulerAngles = new Vector3(0, mouseX, 0);
+
+        if (controller.isGrounded)
+        {
+            mov = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            mov = controller.transform.TransformDirection(mov);
+        }
+        else
+        {
+            mov.y -= gravity * Time.deltaTime;
+        }
+        
+        controller.Move(mov * Time.deltaTime * speed);
+
+        if (controller.isGrounded)
+        {
+            Jump();
+        }
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            verticalVelocity = jumpForce;
+        }
+    }
+}
